333b481160daa1a188fca045848c7107e04fbe08
worldkaizen001 <aigbekelvin@gmail.com>
2022-06-09 14:19:15 +0800

    update

---
+++ lib/controllers/providers/auth-provider.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AuthProvider with ChangeNotifier{
+ //Implement Create user with email and password
+
+  //Implement sign in with email and password
+
+  //Implement sign in with google
+
+  //Implement forget password
+
+  //Implement sign out for both google and email
+
+}
